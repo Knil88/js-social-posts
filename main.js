ed1832c4c8183304320cf10789c2885a74779439
Yari <narducciyarigmail.com>
2022-11-23 22:00:29 +0100

    modifica pagina

--- main.js
+++ main.js
@@ -56,93 +56,91 @@ const posts = [
     }
 ];
 
-let cont = document.getElementById("container");
+
+// Milestone 2 -
+// Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
 
 
-posts.forEach((element)=>{
-    let iduser = element.id;
-
-    let postDate = element.created;
-
-    let postIcon = element.author.image;
-
-    console.log(postIcon);
-
-    let iconAuthor = element.author.name;
-
-    console.log(iconAuthor);
-
-    let imageAuthor = element.media;
-
-    console.log(imageAuthor);
-
-    let likeUser = element.likes;
-    console.log(likeUser);
-
-    let socialData =` <div class="post">
-    <div class="post__header">
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src=${postIcon} alt="Phil Mangione">                    
-            </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">   ${iconAuthor}</div>
-                <div class="post-meta__time">${postDate}</div>
-            </div>                    
-        </div>
-    </div>
-    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-    <div class="post__image">
-        <img src="${imageAuthor}" alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button"  data-postid="${iduser}">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${likeUser}</b> persone
-            </div>
-        </div> 
-    </div>            
-</div>`
-cont.innerHTML+=(socialData);
+// ripercorro tutto l'array per assegnare i vari elementi
+let container = document.getElementById("container");
+let num = 0;
+inner ();
+let btn = document.getElementsByClassName("like-button")
+for (let index = 0; index < btn.length; index++) {
+    const element = btn[index];
+    element.addEventListener("click",
+    function () {
+        element.classList.toggle("like-button--liked")
 
 
-let likeBtn = document.querySelectorAll(".like-button");
-
-console.log(likeBtn);
-
-
-for(let i = 0; i < likeBtn.length; i ++){
-    let elemento = likeBtn[i];
-    elemento.addEventListener("click",
-    function(){
-       
-        elemento.classList.toggle("like-button--liked")
-        elemento.classList.toggle("color");
-        likeUser = posts[i].likes;
-        iduser = i + 1; 
-        let counter = document.getElementById("like-counter-" + iduser);
-        counter.innerHTML = likeUser + 1;
-        elemento.classList.add("like-button--liked")
+        let likes = posts[index].likes
+        let id = index + 1;
+        let counter = document.getElementById("like-counter-"+id )
         const idArray = [];
-        idArray.push(iduser);
-        console.log(idArray);
 
-    }
-);   
+        if (num === 0) {
+            num++;
+            counter.innerHTML = likes + num;
+            idArray.push(id);
+            console.log(idArray);
+        } else if (num === 1){
+            num--;
+            counter.innerHTML = likes + num;
+
+        }
+      
+        
+
+        
+
+    })
+    
 }
 
 
+function inner() {
+    posts.forEach(element => {
 
+        let count = parseInt(element.likes);
+        let post = document.createElement("div");
+        post.classList.add("post");
+        post.innerHTML = "";
+        let data = element.created;
+        data = parseInt(data.replace('2021-',''));
+        if (element.author.image == null) {
+            nameSplit = element.author.name.split(" ");
+            text = nameSplit[0].charAt(0) + nameSplit[1].charAt(0) ;
+        }
+        post.innerHTML =`
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src=${element.author.image} alt=${element.author.image == null ? text : element.author.image}>                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${element.author.name}</div>
+                            <div class="post-meta__time">${12-data} mesi fa</div>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="post__text">${element.content}</div>
+                <div class="post__image">
+                    <img src=${element.media} alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#!" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${count}</b> persone
+                        </div>
+                    </div> 
+                </div>            
+        `
+        container.append(post)
+    });
 }
-)
-
-
-
-
-
